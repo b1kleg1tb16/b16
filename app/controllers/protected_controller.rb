b7e672503e36698179bc63595d4d0769562f3afc
maco green <b1kleg1tb16@yahoo.com>
2008-08-24 13:33:22 -0700

    first commit

---
+++ app/controllers/protected_controller.rb
@@ -0,0 +1,17 @@
+class ProtectedController < ApplicationController
+  def index
+  end
+
+  def about
+  end
+
+  def services
+  end
+
+  def contact
+  end
+
+  def tech_tips
+  end
+
+end
---
+++ public/javascripts/sql.js
@@ -0,0 +1,14 @@
+CodeHighlighter.addStyle("sql",{
+        comment : {
+                exp  : /(--[^\n]+)|(\/\*[^*]*\*+([^\/][^*]*\*+)*\/)/
+        },
+        brackets : {
+                exp  : /\(|\)/
+        },
+        string : {
+                exp  : /'[^']*'|"[^"]*"/
+        },
+        symbol : {
+                exp  : /\b(SELECT|INSERT|UPDATE|DELETE|MERGE|WHERE|AND|OR|FROM|CREATE|TABLE|NUMBER|TABLESPACE|INDEX|VIEW|AS|GROUP|ORDER|BY|MAX|MIN|AVG|COUNT|ABS|UNION|COLUMN|FORMAT|SET|RULES)\b/
+        }
+});
