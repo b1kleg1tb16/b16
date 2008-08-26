391865c7b4c2b69decec2449104a39ce21543d38
maco green <b1kleg1tb16@yahoo.com>
2008-08-26 15:53:01 -0700

    Added some files

---
+++ gaddem.sh
@@ -0,0 +1,17 @@
+
+git add   app/views/layouts/application.html.erb
+git add   app/views/protected/contact.html.erb
+git add   app/views/protected/services.html.erb
+git add   app/views/protected/tech_tips.html.erb
+git add   public/old_site.html
+git add        app/views/protected/o10gR2_solaris10x86.html.erb
+git add        app/views/protected/roracle_session.html.erb
+git add        app/views/protected/tech_tips.rhtml
+git add        gaddem.sh
+git add        public/images/dan8611.jpg
+git add        public/images/et_email.jpg
+git add        public/images/tech_tips.jpg
+
+git commit -v -m  "Added some files"
+git push origin master
+
