a55a1949b12611e0180ba14134f02b32bea17377
maco green <b1kleg1tb16@yahoo.com>
2008-08-29 16:11:01 -0700

    git add   app/views/protected/o10gR2_solaris10x86.html.erb , app/views/protected/roracle_session.html.erb , public/javascripts/sql.js

--- gaddem.sh
+++ gaddem.sh
@@ -1,17 +1,9 @@
 
-git add   app/views/layouts/application.html.erb
-git add   app/views/protected/contact.html.erb
-git add   app/views/protected/services.html.erb
-git add   app/views/protected/tech_tips.html.erb
-git add   public/old_site.html
-git add        app/views/protected/o10gR2_solaris10x86.html.erb
-git add        app/views/protected/roracle_session.html.erb
-git add        app/views/protected/tech_tips.rhtml
-git add        gaddem.sh
-git add        public/images/dan8611.jpg
-git add        public/images/et_email.jpg
-git add        public/images/tech_tips.jpg
 
-git commit -v -m  "Added some files"
+git add   app/views/protected/o10gR2_solaris10x86.html.erb
+git add   app/views/protected/roracle_session.html.erb
+git add   gaddem.sh
+git add   public/javascripts/sql.js
+
+git commit -v -m  "git add   app/views/protected/o10gR2_solaris10x86.html.erb , app/views/protected/roracle_session.html.erb , public/javascripts/sql.js"
 git push origin master
-
