f753c6faeaa843c25f90cbc91e6b78da226fd6c1
maco green <b1kleg1tb16@yahoo.com>
2008-08-30 12:15:01 -0700

    Added favicon.ico which gives red B, added caches_page calls to protected_controller.rb

--- app/controllers/protected_controller.rb
+++ app/controllers/protected_controller.rb
@@ -1,5 +1,13 @@
 class ProtectedController < ApplicationController
 
+  caches_page :index
+  caches_page :about
+  caches_page :services
+  caches_page :contact
+  caches_page :tech_tips
+  caches_page :o10gR2_solaris10x86
+  caches_page :roracle_session
+
   def index
   end
 
@@ -13,6 +21,12 @@ class ProtectedController < ApplicationController
   end
 
   def tech_tips
+  end
+
+  def  o10gR2_solaris10x86
+  end
+
+  def  roracle_session
   end
 
 end
