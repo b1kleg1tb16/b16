0f7b194b0f8a9ec225b16946c3bf6b00c00ec894
maco green <b1kleg1tb16@yahoo.com>
2008-08-30 01:57:09 -0700

    git add    db/mysql_ins_statements2008_0829.sql

--- gaddem.sh
+++ gaddem.sh
@@ -1,9 +1,7 @@
 
+git add    db/mysql_ins_statements2008_0829.sql
+git add    gaddem.sh
 
-git add   app/views/protected/o10gR2_solaris10x86.html.erb
-git add   app/views/protected/roracle_session.html.erb
-git add   gaddem.sh
-git add   public/javascripts/sql.js
-
-git commit -v -m  "git add   app/views/protected/o10gR2_solaris10x86.html.erb , app/views/protected/roracle_session.html.erb , public/javascripts/sql.js"
+git commit -v -m  "git add    db/mysql_ins_statements2008_0829.sql"
 git push origin master
+
