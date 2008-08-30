79902ecabf3ecffeec4691733e40082bfe1708b7
maco green <b1kleg1tb16@yahoo.com>
2008-08-30 12:01:12 -0700

    Added overflow divs and google ads

--- app/controllers/protected_controller.rb
+++ app/controllers/protected_controller.rb
@@ -1,4 +1,5 @@
 class ProtectedController < ApplicationController
+
   def index
   end
 
--- gaddem.sh
+++ gaddem.sh
@@ -1,7 +1,11 @@
 
-git add    db/mysql_ins_statements2008_0829.sql
+git add    app/controllers/protected_controller.rb
+git add    app/views/layouts/application.html.erb
+git add    app/views/protected/o10gR2_solaris10x86.html.erb
+git add    app/views/protected/roracle_session.html.erb
 git add    gaddem.sh
 
-git commit -v -m  "git add    db/mysql_ins_statements2008_0829.sql"
+git commit -v -m  "Added overflow divs and google ads"
 git push origin master
+
 
