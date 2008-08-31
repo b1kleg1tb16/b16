944ff66a31c3676b47763d391a98d1191efc41de
maco green <b1kleg1tb16@yahoo.com>
2008-08-31 14:49:38 -0700

    enhanced: sitecode.html.erb , layout, syntax.css. added: javascript.js

--- gaddem.sh
+++ gaddem.sh
@@ -1,9 +1,9 @@
 
-git add    app/controllers/protected_controller.rb
+git add    app/views/protected/about.html.erb
+git add    app/views/protected/roracle_session.html.erb
 git add    app/views/protected/sitecode.html.erb
-git add    app/views/protected/tech_tips.html.erb
 git add    gaddem.sh
 
-git commit -v -m  "Final 1st draft of sitecode now done"
+git commit -v -m  "enhanced: sitecode.html.erb , layout, syntax.css. added: javascript.js"
 git push origin master
 
