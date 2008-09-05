973d0a524118fc96879a74ed25beadb6905604ed
maco green <b1kleg1tb16@yahoo.com>
2008-09-05 11:50:17 -0700

    enhanced app/views/protected/mvg_avg_study.html.erb, removed un-needed div layer from layout, added scroll-bar to AS views, removed word wrap from pre-tag CSS in application.css

--- gaddem.sh
+++ gaddem.sh
@@ -1,9 +1,10 @@
 
-git add    app/views/protected/about.html.erb
-git add    app/views/protected/roracle_session.html.erb
-git add    app/views/protected/sitecode.html.erb
-git add    gaddem.sh
+git add   app/views/layouts/application.html.erb
+git add   app/views/protected/mvg_avg_study.html.erb
+git add   gaddem.sh
+git add   public/stylesheets/active_scaffold_overrides.css
+git add   public/stylesheets/application.css
 
-git commit -v -m  "enhanced: sitecode.html.erb , layout, syntax.css. added: javascript.js"
+git commit -v -m  "enhanced app/views/protected/mvg_avg_study.html.erb, removed un-needed div layer from layout, added scroll-bar to AS views, removed word wrap from pre-tag CSS in application.css"
 git push origin master
 
